80091d1980047dd3db73afbdfb770ead2cd99f68
Hubert <hubert.zajac92@gmail.com>
2023-06-04 17:50:46 +0200

    Add dummy data

---
+++ src/data/featuredHousesList.ts
@@ -0,0 +1,98 @@
+export const featuredHousesList = [
+  {
+    id: 1,
+    title: 'Serenity Manor',
+    price: 1200000,
+    owner: 'James Anderson',
+    city: 'San Francisco',
+    imgOwner:
+      'https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    imgHouse:
+      'https://images.unsplash.com/photo-1570129477492-45c003edd2be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    status: 'popular',
+  },
+  {
+    id: 2,
+    title: 'Willow Creek Retreat',
+    price: 850000,
+    owner: 'Richard Thompson',
+    city: 'Seattle',
+    imgOwner:
+      'https://images.unsplash.com/photo-1504593811423-6dd665756598?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    imgHouse:
+      'https://images.unsplash.com/photo-1568605114967-8130f3a36994?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    status: 'newHouse',
+  },
+  {
+    id: 3,
+    title: 'Oak Ridge Estate',
+    price: 2500000,
+    owner: 'Richard Harrison',
+    city: 'Chicago',
+    imgOwner:
+      'https://images.unsplash.com/photo-1500048993953-d23a436266cf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80',
+    imgHouse:
+      'https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1175&q=80',
+    status: 'bestDeals',
+  },
+  {
+    id: 4,
+    title: 'Coastal Breeze Haven',
+    price: 1600000,
+    owner: 'Jennifer Roberts',
+    city: 'Miami',
+    imgOwner:
+      'https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+    imgHouse:
+      'https://images.unsplash.com/photo-1564013799919-ab600027ffc6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    status: 'popular',
+  },
+  {
+    id: 5,
+    title: 'Mountain View Villa',
+    price: 1750000,
+    owner: 'Michael Johnson',
+    city: 'Denver',
+    imgOwner:
+      'https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    imgHouse:
+      'https://images.unsplash.com/photo-1570129477492-45c003edd2be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    status: 'popular',
+  },
+  {
+    id: 6,
+    title: 'Whispering Pines Cottage',
+    price: 550000,
+    owner: 'Jessica Parker',
+    city: 'Asheville',
+    imgOwner:
+      'https://images.unsplash.com/photo-1542206395-9feb3edaa68d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
+    imgHouse:
+      'https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    status: 'popular',
+  },
+  {
+    id: 7,
+    title: 'Sunflower Meadows Residence',
+    price: 950000,
+    owner: 'Daniel Adams',
+    city: 'Austin',
+    imgOwner:
+      'https://images.unsplash.com/photo-1544723795-3fb6469f5b39?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=689&q=80',
+    imgHouse:
+      'https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80',
+    status: 'popular',
+  },
+  {
+    id: 8,
+    title: 'Harmony Haven',
+    price: 1400000,
+    owner: 'Brain Carter',
+    city: 'Nashville',
+    imgOwner:
+      'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+    imgHouse:
+      'https://images.unsplash.com/photo-1598228723793-52759bba239c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
+    status: 'popular',
+  },
+];
---
+++ src/data/tipsList.ts
@@ -0,0 +1,32 @@
+export const tipsList = [
+  {
+    id: 1,
+    imgHouse:
+      "https://images.unsplash.com/photo-1616137466211-f939a420be84?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80",
+    imgPerson:
+      "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    personName: "Adam Nowak",
+    title: "Co musisz sprawdzić przed zakupem mieszkania",
+    date: "4 min czytania | 25 Mar 2022",
+  },
+  {
+    id: 2,
+    imgHouse:
+      "https://images.unsplash.com/photo-1564013799919-ab600027ffc6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    imgPerson:
+      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    personName: "Jan Kowalski",
+    title: "7 sposobów na sprawdzenie jakości domu",
+    date: "6 min czytania | 22 Mar 2022",
+  },
+  {
+    id: 3,
+    imgHouse:
+      "https://images.unsplash.com/photo-1598228723793-52759bba239c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+    imgPerson:
+      "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
+    personName: "Joanna Szymańska",
+    title: "Najlepsze sposoby na wybór odpowiedniego domu",
+    date: "2min czytania | 18 Mar 2022",
+  },
+];
